63ae667b4e5273369c0aa40e215d0930821078e5
leibeiyi <leibeiyi@coocaa.com>
2025-02-18 10:35:52 +0800

    build(android): 更新 gradle 插件并调整构建配置
    - 更新 com.android.tools.build:gradle 插件到 4.1.0 版本
    - 修改 gradle-wrapper.properties 中的 gradle 分发 URL
    - 调整 android-example 和 android-tests 项目的构建配置
    - 更新 JNIReachabilityFence 类和修改 android-tests 的目标架构

--- android-tests/build.rs
+++ android-tests/build.rs
@@ -27,9 +27,9 @@ fn main() {
     let target = env::var("TARGET").unwrap();
     if [
         "aarch64-linux-android",
-        "arm-linux-androideabi",
-        "i686-linux-android",
-        "x86_64-linux-android",
+        // "arm-linux-androideabi",
+        // "i686-linux-android",
+        // "x86_64-linux-android",
         "armv7-linux-androideabi",
     ]
     .contains(&target.as_str())
